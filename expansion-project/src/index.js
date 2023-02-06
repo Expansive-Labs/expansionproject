023cad3819afedb767b5a439e913084a31fd18f4
Anthony <antharithm707@gmail.com>
2023-02-06 17:17:15 -0500

    click counter

--- expansion-project/src/index.js
+++ expansion-project/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import "98.css";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
