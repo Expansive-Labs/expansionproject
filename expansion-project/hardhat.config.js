5818228440db7fc9008d3b5a30e662c3fd4fc76a
Anthony <antharithm707@gmail.com>
2023-03-12 09:16:52 -0400

    removed greeter

--- expansion-project/hardhat.config.js
+++ expansion-project/hardhat.config.js
@@ -11,11 +11,11 @@ module.exports = {
   networks: {
     goerli: {
 
-      url: process.env.NODE_ENDPOINT,
+      url: process.env.GOERLI_RPC_URL,
       accounts: [process.env.PRIVATE_KEY]
     }
   },
   etherscan: {
-    apiKey: process.env.API_KEY
+    apiKey: process.env.ETHERSCAN_API_KEY
   }
 };
--- expansion-project/scripts/deploy.js
+++ expansion-project/scripts/deploy.js
@@ -2,12 +2,6 @@ const hre = require("hardhat");
 
 async function main() {
 
-  // Greeter contract deployment
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
-  await greeter.deployed();
-  console.log(`Greeter was deployed to: ${greeter.address}`);
-
   // Expansion Project deployment
   const ExpansionProject = await hre.ethers.getContractFactory("ExpansionProject");
   const expansionProject = await ExpansionProject.deploy();
