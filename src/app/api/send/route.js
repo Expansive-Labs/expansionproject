8dd86abfff06558fda2fce805ac2fe4a51dabce5
Anthony <antharhythm@Anthonys-MacBook-Pro.local>
2023-10-01 10:51:34 -0400

    vercel muted

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,6 +1,7 @@
 // import { EmailTemplate } from '../../../components/EmailTemplate';
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
+// VERCEL
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
