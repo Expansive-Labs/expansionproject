4c1e2c436de4b811d4fb22c5c294299e89f3c11f
Anthony <antharhythm@Anthonys-MacBook-Pro.local>
2023-10-04 18:45:14 -0400

    footer update

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,9 +1,35 @@
-import Image from "next/image";
+// import Image from "next/image";
 
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="container p-12 flex justify-center">
+      <div className="container p-12 justify-center">
+        {/* EXPANSIVE LABS */}
+        <div style={{ position: "relative", top: "-10px", margin: "10px 0" }}>
+          <h1
+            style={{
+              fontFamily: "Press Start 2P",
+              // paddingTop: "16px",
+              fontSize: "9px",
+              color: "#717171",
+            }}
+          >
+            This webpage is powered by
+            <a
+              href="https://github.com/Expansive-Labs"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{
+                color: "#09846d",
+                textDecoration: "none",
+                letterSpacing: "2px",
+              }}
+            >
+              {" "}
+              ///EXPANSIVE LABS///
+            </a>
+          </h1>
+        </div>
         <span className="text-muted"></span>
         <p className="text-slate-700">All rights reserved.</p>
       </div>
