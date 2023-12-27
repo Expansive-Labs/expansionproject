8cb836ecaca0f5785abf79f30f82ab6a68c9e698
Antharithm <antharithm707@gmail.com>
2023-12-27 10:41:57 -0500

    code refactor

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,28 +6,25 @@ import ProjectsSection from "./components/ProjectsSection";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
 import AchievementsSection from "./components/AchievementsSection";
-import { ParallaxProvider } from "react-scroll-parallax";
 import BackToTopButton from "./components/BackToTopButton";
-import Providers from "./providers";
 import MerchCarousel from "./components/MerchCarousel";
+import { ParallaxProvider } from "react-scroll-parallax";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212] dark:bg-[#121212]">
-      <Providers>
-        <Navbar />
-        <div className="container mt-24 mx-auto px-12 py-4 dark:text-[#f6f3ed]">
-          <ParallaxProvider>
-            <HeroSection />
-            <AchievementsSection />
-            <AboutSection />
-            <ProjectsSection />
-            <MerchCarousel />
-            <EmailSection />
-          </ParallaxProvider>
-        </div>
-        <Footer />
-      </Providers>
+      <Navbar />
+      <div className="container mt-24 mx-auto px-12 py-4 dark:text-[#f6f3ed]">
+        <ParallaxProvider>
+          <HeroSection />
+          <AchievementsSection />
+          <AboutSection />
+          <ProjectsSection />
+          <MerchCarousel />
+          <EmailSection />
+        </ParallaxProvider>
+      </div>
+      <Footer />
       <BackToTopButton />
     </main>
   );
