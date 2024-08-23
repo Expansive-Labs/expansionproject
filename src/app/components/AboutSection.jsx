99df17448d1527696d6b1e949afb2a3a1aea97ee
Antharithm <antharithm707@gmail.com>
2024-08-23 14:54:13 -0400

    parallax image optimized

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -1,7 +1,7 @@
-import React, { useTransition, useState } from "react";
+import React, { useTransition, useState, useEffect } from "react";
 import Image from "next/image";
 import TabButton from "./TabButton";
-import { Parallax } from "react-scroll-parallax";
+import { Parallax, ParallaxProvider } from "react-scroll-parallax";
 import Link from "next/link";
 
 /// ABOUT SECTION ///
@@ -34,6 +34,18 @@ const TAB_DATA = [
 const AboutSection = () => {
   const [tab, setTab] = useState("upcoming");
   const [isPending, startTransition] = useTransition();
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth <= 768); // Adjust this breakpoint as needed
+    };
+
+    checkMobile();
+    window.addEventListener("resize", checkMobile);
+
+    return () => window.removeEventListener("resize", checkMobile);
+  }, []);
 
   const handleTabChange = (id) => {
     startTransition(() => {
@@ -55,64 +67,80 @@ const AboutSection = () => {
   };
 
   return (
-    <section className="text-[#f6f3ed]" id="about">
-      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 xl:gap-16 sm:py-16 xl:px-16">
-        <Parallax speed={10} opacity={[-2, 1, "easeOutBack"]}>
-          <Image
-            src="/images/wissPicLive.webp"
-            alt="Expansion Project"
-            width={500}
-            height={500}
-            style={{
-              borderRadius: "10%",
-              filter: "drop-shadow(0 0 44px #bababa)",
-              marginTop: "-80px",
-            }}
-          />
-        </Parallax>
-        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
-          <h2
-            className="text-4xl text-center font-bold determination-mono-font mb-4 text-[#50fd9a]"
-            style={sectionStyle}
-          >
-            About Us
-          </h2>
-          <p className="text-base text-center text-[#bababa] lg:text-lg determination-mono-font">
-            Expansion Project grew out of the passion longtime friends Anthony
-            Tilotta and Matthew Silva have for creating music. Zamere Williams
-            joined the band in 2016, solidifying this trio. Their sound draws
-            inspiration from many sources, forming a unique sonic identity.
-            Studio recordings have captured their creative essence on tape while
-            their live shows are events for collaboration and a raw passion for
-            the love of live music &infin;
-          </p>
-          <p className="text-center text-4xl font-bold text-[#50fd9a] mt-12 mb-0 md:mb-2 determination-mono-font">
-            Shows
-          </p>
-
-          <div className="flex flex-row justify-center mt-8 tab-buttons-container">
-            <TabButton
-              selectTab={() => handleTabChange("upcoming")}
-              active={tab === "upcoming"}
+    <ParallaxProvider>
+      <section className="text-[#f6f3ed]" id="about">
+        <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 xl:gap-16 sm:py-16 xl:px-16">
+          {isMobile ? (
+            <Image
+              src="/images/wissPicLive.webp"
+              alt="Expansion Project"
+              width={500}
+              height={500}
+              style={{
+                borderRadius: "10%",
+                filter: "drop-shadow(0 0 44px #bababa)",
+                marginTop: "-80px",
+              }}
+            />
+          ) : (
+            <Parallax speed={10} opacity={[-2, 1, "easeOutBack"]}>
+              <Image
+                src="/images/wissPicLive.webp"
+                alt="Expansion Project"
+                width={500}
+                height={500}
+                style={{
+                  borderRadius: "10%",
+                  filter: "drop-shadow(0 0 44px #bababa)",
+                  marginTop: "-80px",
+                }}
+              />
+            </Parallax>
+          )}
+          <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+            <h2
+              className="text-4xl text-center font-bold determination-mono-font mb-4 text-[#50fd9a]"
+              style={sectionStyle}
             >
-              {" "}
-              Upcoming{" "}
-            </TabButton>
-            <TabButton
-              selectTab={() => handleTabChange("previous")}
-              active={tab === "previous"}
-            >
-              {" "}
-              Previous{" "}
-            </TabButton>
-          </div>
+              About Us
+            </h2>
+            <p className="text-base text-center text-[#bababa] lg:text-lg determination-mono-font">
+              Expansion Project grew out of the passion longtime friends Anthony
+              Tilotta and Matthew Silva have for creating music. Zamere Williams
+              joined the band in 2016, solidifying this trio. Their sound draws
+              inspiration from many sources, forming a unique sonic identity.
+              Studio recordings have captured their creative essence on tape
+              while their live shows are events for collaboration and a raw
+              passion for the love of live music &infin;
+            </p>
+            <p className="text-center text-4xl font-bold text-[#50fd9a] mt-12 mb-0 md:mb-2 determination-mono-font">
+              Shows
+            </p>
 
-          <div className="mt-8">
-            {TAB_DATA.find((t) => t.id === tab).content}
+            <div className="flex flex-row justify-center mt-8 tab-buttons-container">
+              <TabButton
+                selectTab={() => handleTabChange("upcoming")}
+                active={tab === "upcoming"}
+              >
+                {" "}
+                Upcoming{" "}
+              </TabButton>
+              <TabButton
+                selectTab={() => handleTabChange("previous")}
+                active={tab === "previous"}
+              >
+                {" "}
+                Previous{" "}
+              </TabButton>
+            </div>
+
+            <div className="mt-8">
+              {TAB_DATA.find((t) => t.id === tab).content}
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </ParallaxProvider>
   );
 };
 
